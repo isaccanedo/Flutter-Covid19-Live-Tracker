472ac762c715659c42331470cca009227504cfa3
isaccanedo <isaccanedo@gmail.com>
2020-12-30 15:02:18 -0300

    :smile: Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:covid19/screens/tracker.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Isac Canedo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Tracker(),
+    );
+  }
+}
